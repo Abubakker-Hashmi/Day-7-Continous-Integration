2ce59b6497d2302c48b10b53446294f83bd92646
Hashim <abubakkerhashmi123@gmail.com>
2025-02-19 13:01:16 +0500

    ci implemented 2

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ fifth_power = n ** 5
 # Display results
 st.write(f"The square of {n} is: {square}")
 st.write(f"The cube of {n} is: {cube}")
-st.write(f"The fifth power of {n} is: {fifth_power}") 
+st.write(f"The fifth power of {n} is: {fifth_power}")    #create github folder for githubactions inside workflows
